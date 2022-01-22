682c03757157d37afbda9cadf27178360d741063
ysfmetu <ysf_metu@hotmail.com>
2022-01-22 17:09:53 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,7 +29,7 @@ export class AppComponent {
     }
     );
     obs1.subscribe({
-      next:value=>{console.log('--> '+value)}    
+      next:value=>{console.log('-> '+value)}    
     }
     );
     
